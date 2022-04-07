a4c2067dba07a13a492f5808330d9721dfecdba7
hodokim <hodo323@naver.com>
2022-04-07 23:23:44 +0900

    getting started Express

---
+++ server.js
@@ -0,0 +1,19 @@
+const express =  require('express');
+const app = express();
+
+app.listen(8080, function(){
+    console.log('listening... 8080');
+});
+
+app.get('/pet', function(req, res){
+    res.send('잘 왔어요. 펫 용품 샵이에요');
+});
+
+app.get('/beauty', function (req, res) {
+    res.send('뷰티용품... 사지마세요.');
+});
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
