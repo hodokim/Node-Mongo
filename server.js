3ee28bbb07c6e5437467f53f610525b71accdee4
hodokim <hodo323@naver.com>
2022-04-09 23:52:52 +0900

    bodyParser

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express =  require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended : true}))
 
 app.listen(8080, function(){
     console.log('listening... 8080');
@@ -20,4 +22,9 @@ app.get('/', function (req, res) {
 app.get('/write', function (req, res) {
     res.sendFile(__dirname + '/write.html');
 });
+
+app.post('/add', (req,res) => {
+    res.send('전송완료');
+    console.log(req.body.title);
+})
 
